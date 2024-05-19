48cf3650de39630dba428ceb238b69cfc73e13c4
nivesayee <nivesayee@gmail.com>
2024-05-19 17:06:13 +0100

    Update scraping code(WIP)

---
+++ src/utils/config.py
@@ -0,0 +1,5 @@
+BASE_URL = 'https://pinchofyum.com/'
+RECIPE_LIST_URL = f'{BASE_URL}/recipes/all'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'
+}
