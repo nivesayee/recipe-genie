8fd4485f9dc2081244097472b8b602bec86759cc
nivesayee <nivesayee@gmail.com>
2024-06-11 22:57:45 +0100

    Updated for render

--- src/app.py
+++ src/app.py
@@ -3,18 +3,31 @@ import pickle
 from utils.preprocess import preprocess_user_ingredients
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+import os
 
 
 app = Flask(__name__)
 
+# Get the directory of the current Python script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Go up one directory to access the data directory
+data_dir = os.path.join(current_dir, "..", "data")
+
+# Load all the pickle files using relative paths
+recipes_path = os.path.join(data_dir, "processed", "recipes.pkl")
+tfidf_matrix_path = os.path.join(data_dir, "processed", "tfidf_matrix.pkl")
+vectorizer_path = os.path.join(data_dir, "processed", "vectorizer.pkl")
+
+
 # Load all the pickle files
-with open("../data/processed/recipes.pkl", 'rb') as file:
+with open(recipes_path, 'rb') as file:
    recipe_df = pickle.load(file)
 
-with open("../data/processed/tfidf_matrix.pkl", 'rb') as file:
+with open(tfidf_matrix_path, 'rb') as file:
     tfidf_matrix = pickle.load(file)
 
-with open("../data/processed/vectorizer.pkl", 'rb') as file:
+with open(vectorizer_path, 'rb') as file:
    vectorizer = pickle.load(file)
 
 
