9184f9bc1e6647328820adf41d9a35b6b38f6d71
nivesayee <nivesayee@gmail.com>
2024-06-20 12:30:12 +0100

    Updated unwanted words list

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -19,21 +19,9 @@ def preprocess_ingredients(ingredients_str):
                     'gal', 'gallon', 'lb', 'pound', 'ml', 'millilitre', 'g', 'gram',
                     'kg', 'kilogram', 'l', 'liter'
                     ]
-    unwanted_words = ['chop', 'piec', 'cube', 'like', 'grate', 'slice', 'thinli', 'drizzl', 'mild', 'delallo', 'castelvetrano', 'splash', 'shake', 'tast',
-                      'clove', 'bunch', 'cook', 'inch', 'knob', 'fresh', 'pack', 'one', 'two', 'stalk', 'half', 'san', 'marzano',
-                      'dri', 'spici', 'raw', 'jumbo', 'minc', 'larg', 'beaten', 'mediums', 'head', 'low', 'sodium', 'frozen',
-                      'shred', 'jar', 'favorit', 'small', 'box', 'uncook', 'contain', 'medium', 'heavi', 'english', 'smoke',
-                      'sprig', 'dice', 'petit', 'batch', 'air', 'fryer', 'serv', 'block', 'extra', 'firm', 'fine', 'ish',
-                      'refriger', 'ground', 'thi', 'chunk', 'homemad', 'option', 'top', 'packag', 'individu', 'bundl', 'closer',
-                      'leav','melt','readytoserv','premad','bag','zest','pinch','halv','abov','absolut','access',
-                      'add','addin','addit','also','ancient','asianinspir','barefoot','basic','becaus','best','big','bit','bittersweet',
-                      'blend','bold','bought','brand','buy','case','caught','chef','chihuahua','club','coat','cool','corner','could',
-                      'crack','creation','cupboard','cut','dare','dark','dinner','either','els','everyth','extrasharp','eyebal','farm'
-                      ,'fashion','freshli','friendli','garnish','good','hard','happi','heirloom','hollow','huge','ideal','id','idea',
-                      'ingredi','ive','kind','king','koreanstyl','lake','land','layer','le','leftov','life','light','lightli',
-                      'littl','loos','lot','love','lump','made','magic','make','market','mash','massag','meal','mediumlarg','mediumrip',
-                      'mini','miniatur','minut','mix','mixin','mm','montreal','part','nice','never','note','store','storebought',
-                      'random','shortcut','similar','yum','yummi','wan','want','use','thick']
+    unwanted_words = ['chop', 'piec', 'cube', 'grate', 'slice', 'inch', 'one', 'two', 'half', 'larg',
+                      'mediums', 'small', 'medium', 'best', 'big', 'light',
+                      'lightli', 'littl', 'mediumlarg', 'mini', 'thick']
     for ingredient in ingredients_list:
         ingredient = re.sub(r'\(.*\)', '', ingredient).strip()
         ingredient = ingredient.split(',')[0].lower()
